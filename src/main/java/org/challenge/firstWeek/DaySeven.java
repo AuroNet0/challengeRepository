2c8ac2e30ec5da86a90bcb4fdcbe4d0accce8bf3
AuroNet0 <auroneto04@gmail.com>
2025-07-28 17:49:50 -0300

    Almost there, the first week.

---
+++ src/main/java/org/challenge/firstWeek/DaySeven.java
@@ -0,0 +1,35 @@
+package org.challenge.firstWeek;
+
+public class DaySeven {
+
+//    ✅ Day 7 Challenge: FizzBuzz
+//    📘 Problem:
+//    Write a method that prints the numbers from 1 to n.
+//    But for multiples of 3, print "Fizz" instead of the number,
+//    for multiples of 5, print "Buzz",
+//    and for numbers that are multiples of both 3 and 5, print "FizzBuzz".
+
+    public static void main(String[] args) {
+        fizzBuzz(15);
+    }
+
+    public static void fizzBuzz(int n){
+
+        for (int i=1; i<=n ; i++){
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0){
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
+
+}
